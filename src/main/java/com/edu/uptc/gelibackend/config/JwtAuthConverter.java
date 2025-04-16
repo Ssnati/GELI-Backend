1d379c1dbabf3de6b831228766dd8a271dc2cba2
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2025-04-16 18:18:46 -0500

    add realm roles and resource permissions extraction in JwtAuthConverter

--- src/main/java/com/edu/uptc/gelibackend/config/JwtAuthConverter.java
+++ src/main/java/com/edu/uptc/gelibackend/config/JwtAuthConverter.java
@@ -21,6 +21,7 @@ import java.util.Map;
 @Slf4j
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
     private static final String ROLE_PREFIX = "ROLE_";
+    private static final String REALM_ACCESS_CLAIM = "realm_access";
     private static final String RESOURCE_ACCESS_CLAIM = "resource_access";
     private static final String ROLES_CLAIM = "roles";
 
@@ -37,15 +38,31 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
     public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
         this.validateResourceIdClaim();
         Collection<GrantedAuthority> authorities = new ArrayList<>(jwtConverter.convert(jwt));
-        authorities.addAll(this.extractResourceRoles(jwt));
+
+        authorities.addAll(this.extractRealmRoles(jwt));
+        authorities.addAll(this.extractResourcePermissions(jwt));
 
         String principal = jwt.getClaimAsString(principalClaim);
         log.debug("Principal claim: {}, Authorities: {}", principal, authorities);
 
+        System.out.println("Principal claim: " + principal);
+        System.out.println("Authorities: " + authorities);
         return new JwtAuthenticationToken(jwt, authorities, principal);
     }
 
-    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+    private Collection<? extends GrantedAuthority> extractRealmRoles(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaimAsMap(REALM_ACCESS_CLAIM);
+        if (realmAccess == null) return List.of();
+
+        Object roles = realmAccess.get(ROLES_CLAIM);
+        if (!(roles instanceof Collection<?> rolesCollection)) return List.of();
+
+        return rolesCollection.stream()
+                .map(role -> new SimpleGrantedAuthority(ROLE_PREFIX + role))
+                .toList();
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourcePermissions(Jwt jwt) {
         Map<String, Object> resourceAccess = jwt.getClaimAsMap(RESOURCE_ACCESS_CLAIM);
         if (resourceAccess == null) {
             return List.of();
@@ -62,7 +79,7 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
         }
 
         return rolesCollection.stream()
-                .map(role -> new SimpleGrantedAuthority(ROLE_PREFIX + role))
+                .map(role -> new SimpleGrantedAuthority(String.valueOf(role)))
                 .toList();
     }
 
