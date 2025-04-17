36708abe712e528286bebd1cf10c4f21d15eb729
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2025-04-17 00:36:38 -0500

    entities to quipament and function

---
+++ src/main/java/com/edu/uptc/gelibackend/entities/EquipmentEntity.java
@@ -0,0 +1,44 @@
+package com.edu.uptc.gelibackend.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "equipment")
+public class EquipmentEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "equipment_name", nullable = false)
+    private String equipmentName;
+
+    @NotNull
+    @Column(name = "brand", nullable = false)
+    private String brand;
+
+    @NotNull
+    @Column(name = "inventory_number", nullable = false)
+    private String inventoryNumber;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "laboratory_location_id", nullable = false)
+    private LaboratoryEntity laboratory;
+
+    @NotNull
+    @Column(name = "equipment_availability", nullable = false)
+    private Boolean availability;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "function_id", nullable = false)
+    private FunctionEntity function;
+}
---
+++ src/main/java/com/edu/uptc/gelibackend/entities/FunctionEntity.java
@@ -0,0 +1,24 @@
+package com.edu.uptc.gelibackend.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "function")
+public class FunctionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "function_name", nullable = false)
+    private String functionName;
+
+}
