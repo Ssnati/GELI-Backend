7ea5d58759e995e4d86b14faa856fdea3d872b54
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2025-04-27 15:14:05 -0500

    agregada validación y nuevos campos en la entidad EquipmentUseEntity

--- src/main/java/com/edu/uptc/gelibackend/entities/EquipmentUseEntity.java
+++ src/main/java/com/edu/uptc/gelibackend/entities/EquipmentUseEntity.java
@@ -1,6 +1,7 @@
 package com.edu.uptc.gelibackend.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -18,23 +19,44 @@ public class EquipmentUseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "equipment_id", nullable = false)
     private EquipmentEntity equipment;
 
-    @ManyToOne
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
+
+    @NotNull
+    @Column(name = "use_date", nullable = false)
     private LocalDate useDate;
+
+    @NotNull
+    @Column(name = "start_use_time", nullable = false)
     private LocalTime startUseTime;
+
+    @Column(name = "end_use_time")
     private LocalTime endUseTime;
+
+    @NotNull
+    @Column(name = "status", nullable = false)
     private Boolean status;
+
+    @NotNull
+    @Column(name = "samples_number", nullable = false)
     private int samplesNumber;
-    
+
     @OneToMany(mappedBy = "equipment", cascade = CascadeType.ALL)
     @ToString.Exclude
     private List<EquipmentFunctionsUsedEntity> equipmentFunctionsUsedList;
-    
+
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "applicant_id", nullable = false)
     private ApplicantEntity applicant;
+
+    @Column(name = "observations")
     private String observations;
 }
